4dcf2910bf13ead70c0867d8a95a0c4cd9194670
Peter Lyons Kehl <peter.kehl@gmail.com>
2024-12-08 10:28:08 -0800

    nightly (optional) features

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 // Copyright 2019 DFINITY
+// Copyright 2024 Peter Lyons Kehl
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,10 +15,22 @@
 
 #![no_std]
 
+#![cfg_attr(any(feature = "unstable_generic_const_own_type", feature="unstable_transmute_unchecked"), allow(incomplete_features))]
+
+#![cfg_attr(feature = "unstable_generic_const_own_type", feature(adt_const_params), feature(generic_const_exprs))]
+
+#![cfg_attr(feature="unstable_transmute_unchecked", feature(core_intrinsics))]
+
+//#![feature(unsized_const_params)] // https://github.com/rust-lang/rust/issues/95174
+
 mod amount;
 mod displayer;
 mod id;
 mod instant;
+//pub mod prelude;
+//pub mod prelude_full;
+//mod to;
+//mod trait_flag;
 
 pub use amount::Amount;
 pub use displayer::{DisplayProxy, DisplayerOf};
